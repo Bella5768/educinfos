c16eeb044b3c0a79c9e4388bd439da93510e72d6
Bella5768 <bell@csig.edu.gn>
2026-09-06 16:05:35 +0000

    Add Django CMS for visual content management

--- educinfos/urls.py
+++ educinfos/urls.py
@@ -8,7 +8,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('articles.urls')),
+    path('', include('cms.urls')),
+    path('articles/', include('articles.urls')),
 ]
 
 if settings.DEBUG:
